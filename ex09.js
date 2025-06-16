4e55305189cabf687f6dcf507d3c7e20141dfdc7
BiscuitCuit15 <145538077+LUCA-DesComBes@users.noreply.github.com>
2025-06-16 15:01:44 +0200

    feat (app) : ex09 finish

--- ex09.js
+++ ex09.js
@@ -1,11 +1,10 @@
 function supprimerDoublons(tab) {
-    let acc = tab;
-    let val = [];
-    for (let i = 0; i < tab.length; i++) {
-        if (acc.indexOf(val) === -1) {
-            acc.push(val)
-        } 
-    }
-    return acc;
+	let acc = [];
+	for (const value of tab) {
+		if (acc.indexOf(value) === -1) {
+            acc.push(value)
+        }
+	}
+	return acc
 }
 console.log(supprimerDoublons([1, 2, 2, 3, 3, 3])); // Résultat attendu : [1, 2, 3]
